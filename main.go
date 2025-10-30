cbb3c11d9dc3984e39ece7774149eb38e6ae8605
lcaparada <lcaparada@gmail.com>
2025-10-30 11:18:33 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"github.com/lcaparada/gopportunities/config"
 	"github.com/lcaparada/gopportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		panic(err)
+	}
 
 	router.Initialize()
 }
